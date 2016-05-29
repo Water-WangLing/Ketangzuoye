6e04141671b099dd3da26a7a507f42903d2326f6
Water-WangLing <1047849567@qq.com>
2016-05-29 16:40:04 +0800

    first homework

---
+++ Animal.java
@@ -0,0 +1,19 @@
+package com.baidu.wgz.test;
+
+ class Animal {
+
+	 String name;
+	
+	 String sex;
+	
+	 Animal() {
+		
+	}
+
+
+
+	 void Sleep(){
+		
+		System.out.println(this.name+"Ë¯¾õ");
+	}
+}
